3a0aa3248a4adc9833f0444067101a012a69aa54
Cameron Bisca <cjbisca@gmail.com>
2020-11-06 00:31:00 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -40,16 +40,16 @@ def add_characters
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
     },
-      :capulet => {
-        :patriarch => {name: "Lord Capulet", age: "50"},
-        :matriarch => {name: "Lady Capulet", age: "51"},
-        :heroine => {name: "Juliet", age: "15", status: "alive"},
-        :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
-         ]
-      }
-   }
+    :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+    }
+  
   epic_tragedy[:capulet][:heroine][:status] = "dead"
    
    
