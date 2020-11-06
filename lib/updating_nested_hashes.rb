729443cad1ca2955b39dd961f6a633e837521b0e
Cameron Bisca <cjbisca@gmail.com>
2020-11-06 00:48:55 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -55,10 +55,8 @@ def add_characters
 }
   
 
-  epic_tragedy[:additional_characters] = [
-  { name: "Prince Escalus" },
-  { name: "Apothecary" }
-]
+ epic_tragedy[:additional_characters] = [
+   ]
 
    # After your code, the following line will return the altered hash
   puts epic_tragedy
